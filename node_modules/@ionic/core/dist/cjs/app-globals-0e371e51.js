2916a7e81c75c3b8c1dcef8f93389a592e036f8f
salva09 <salva.hg01@gmail.com>
2020-04-19 19:25:28 -0500

    Removed unnecessary stuff

--- node_modules/@ionic/core/dist/cjs/app-globals-0e371e51.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const ionicGlobal = require('./ionic-global-0dc7ffc4.js');
-
-const globalScripts = ionicGlobal.appGlobalScript;
-
-exports.globalScripts = globalScripts;
--- node_modules/@ionic/core/dist/ionic/p-877e0607.system.js
+++
@@ -1,1 +0,0 @@
-System.register(["./p-0318c166.system.js"],(function(t){"use strict";var e;return{setters:[function(t){e=t.a}],execute:function(){const s=t("g",e)}}}));
