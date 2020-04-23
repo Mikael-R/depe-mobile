4df1e7e27c247df5340da3ef08228ced571836ea
Mikael Rolim de Aquino <60241602+Mikael-R@users.noreply.github.com>
2020-04-22 21:41:39 -0300

    Update TS for Json

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -13,14 +13,108 @@ export class Tab1Page {
 	constructor() { }
 
 	capitalizeFirstLetter(string) {
-	    return String(string).replace(/\w\S*/g, function (txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase() })
+		return String(string).replace(/\w\S*/g, function (txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase() })
 	}
 
 	searchElement(value) {
-            return
+		import * as data from '../PeriodicTableJSON.json'
+
+		if (!isNaN(parseFloat(value))) {
+			for (let elements of data.elements) {
+				if (value == elements.number) {
+					return {
+						"properties": [
+							{
+								"name": elements.name,
+								"symbol": elements.symbol,
+								"number": elements.number,
+								"atomic_mass": elements.atomic_mass,
+								"density": elements.density,
+								"summary": elements.summary,
+								"xpos": elements.xpos,
+								"ypos": elements.ypos,
+								"shells": elements.shells,
+								"electron_configuration": elements.electron_configuration,
+								"melt": elements.melt,
+								"boil": elements.boil,
+								"category": elements.category,
+								"phase": elements.phase,
+								"appearance": elements.appearance,
+								"color": elements.color,
+								"discovered_by": elements.discovered_by
+							}
+						]
+					}
+				}
+			}
+		}
+		else if (value.length > 3 && isNaN(value)) {
+			for (let elements of data.elements) {
+				if (value == elements.name) {
+					return {
+						"properties": [
+							{
+								"name": elements.name,
+								"symbol": elements.symbol,
+								"number": elements.number,
+								"atomic_mass": elements.atomic_mass,
+								"density": elements.density,
+								"summary": elements.summary,
+								"xpos": elements.xpos,
+								"ypos": elements.ypos,
+								"shells": elements.shells,
+								"electron_configuration": elements.electron_configuration,
+								"melt": elements.melt,
+								"boil": elements.boil,
+								"category": elements.category,
+								"phase": elements.phase,
+								"appearance": elements.appearance,
+								"color": elements.color,
+								"discovered_by": elements.discovered_by
+							}
+						]
+					}
+				}
+			}
+		}
+		else if (value.length <= 3 && isNaN(value)) {
+			console.log('Symbol')
+			for (let elements of data.elements) {
+				if (value == elements.symbol) {
+					return {
+						"properties": [
+							{
+								"name": elements.name,
+								"symbol": elements.symbol,
+								"number": elements.number,
+								"atomic_mass": elements.atomic_mass,
+								"density": elements.density,
+								"summary": elements.summary,
+								"xpos": elements.xpos,
+								"ypos": elements.ypos,
+								"shells": elements.shells,
+								"electron_configuration": elements.electron_configuration,
+								"melt": elements.melt,
+								"boil": elements.boil,
+								"category": elements.category,
+								"phase": elements.phase,
+								"appearance": elements.appearance,
+								"color": elements.color,
+								"discovered_by": elements.discovered_by
+							}
+						]
+					}
+				}
+			}
+		}
+		else {
+			return null
+		}
 	}
 
 	buttonClick(value) {
-		alert(`Clicked, ${this.inputValue}`)
+		let element = this.searchElement(this.capitalizeFirstLetter(value))
+		alert(element.properties[0])
+                //alert('Click')
 	}
 }
