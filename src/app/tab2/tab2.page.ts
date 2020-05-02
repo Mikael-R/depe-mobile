a323fe85dc8164df2c82f78b97b2850be6f3cbe5
Mikael-R <mikarg9@gmail.com>
2020-05-02 20:06:08 -0300

    Refactored code, changed information and icon

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,60 +6,20 @@ import * as data from 'src/app/PeriodicTable.json';
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
 
-	public input_value_tab2: string = null;
-	public element_name_label_tab2: string = null;
-	public element_symbol_label_tab2: string = null;
-	public element_number_label_tab2: string = null;
-	public element_mass_label_tab2: string = null;
-	public element_density_label_tab2: string = null;
-	public element_summary_label_tab2: string = null;
-	public element_category_label_tab2: string = null;
-	public element_phase_label_tab2: string = null;
-	public element_appearance_label_tab2: string = null;
-	public element_discovered_by_label_tab2 = null;
-	public element_melt_label_tab2 = null;
-	public element_boil_label_tab2 = null;
-	public element_xpos_label_tab2 = null;
-	public element_ypos_label_tab2 = null;
+export class Tab2Page {
+	public input_value: string = null
+    public element: any = ''
 
 	constructor() {}
 
 	updateLabel(element) {
 		if (element == null) {
 			alert('The value does not belong to any element!')
-			this.input_value_tab2 = null;
-			this.element_name_label_tab2 = null;
-			this.element_symbol_label_tab2 = null;
-			this.element_number_label_tab2 = null;
-			this.element_mass_label_tab2 = null;
-			this.element_density_label_tab2 = null;
-			this.element_summary_label_tab2 = null;
-			this.element_category_label_tab2 = null;
-			this.element_phase_label_tab2 = null;
-			this.element_appearance_label_tab2 = null;
-			this.element_discovered_by_label_tab2 = null;
-			this.element_melt_label_tab2 = null;
-			this.element_boil_label_tab2 = null;
-			this.element_xpos_label_tab2 = null;
-			this.element_ypos_label_tab2 = null;
+			this.element = ''
 		}
 		else {
-			this.element_name_label_tab2 = element.name;
-			this.element_symbol_label_tab2 = element.symbol;
-			this.element_number_label_tab2 = element.number;
-			this.element_mass_label_tab2 = element.atomic_mass;
-			this.element_density_label_tab2 = element.density;
-			this.element_summary_label_tab2 = element.summary;
-			this.element_category_label_tab2 = element.category;
-			this.element_phase_label_tab2 = element.phase;
-			this.element_appearance_label_tab2 = element.appearance;
-			this.element_discovered_by_label_tab2 = element.discovered_by;
-			this.element_melt_label_tab2 = element.melt;
-			this.element_boil_label_tab2 = element.boil;
-			this.element_xpos_label_tab2 = element.xpos;
-			this.element_ypos_label_tab2 = element.ypos;
+			this.element = element
 		}
 	}
 
@@ -68,97 +28,26 @@ export class Tab2Page {
 	}
 
 	searchElement(value) {
-		if (!isNaN(parseFloat(value))) {
-			for (let elements of data.elements) {
-				if (value == elements.number) {
-					return {
-						properties: [
-							{
-								name: elements.name,
-								symbol: elements.symbol,
-								number: elements.number,
-								atomic_mass: elements.atomic_mass,
-								density: elements.density,
-								summary: elements.summary,
-								category: elements.category,
-								phase: elements.phase,
-								appearance: elements.appearance,
-								discovered_by: elements.discovered_by,
-								melt: elements.melt,
-								boil: elements.boil,
-								xpos: elements.xpos,
-								ypos: elements.ypos	
-							}
-						]
-					}
-				}
-			}
+        let elements = null
+
+        if (!isNaN(value)) {
+            elements = data.elements.filter(el => Number(value) === el.number)
+        }
+        else if (isNaN(value)) {
+
+            if (value.length > 3) {
+                elements = data.elements.filter(el => String(value) === el.name)
+            } else {
+                elements = data.elements.filter(el => String(value) === el.symbol)
+            }
 		}
-		else if (value.length > 3 && isNaN(value)) {
-			for (let elements of data.elements) {
-				if (value == elements.name) {
-					return {
-						properties: [
-							{
-								name: elements.name,
-								symbol: elements.symbol,
-								number: elements.number,
-								atomic_mass: elements.atomic_mass,
-								density: elements.density,
-								summary: elements.summary,
-								category: elements.category,
-								phase: elements.phase,
-								appearance: elements.appearance,
-								discovered_by: elements.discovered_by,
-								melt: elements.melt,
-								boil: elements.boil,
-								xpos: elements.xpos,
-								ypos: elements.ypos	
-							}
-						]
-					}
-				}
-			}
-		}
-		else if (value.length <= 3 && isNaN(value)) {
-			for (let elements of data.elements) {
-				if (value == elements.symbol) {
-					return {
-						properties: [
-							{
-								name: elements.name,
-								symbol: elements.symbol,
-								number: elements.number,
-								atomic_mass: elements.atomic_mass,
-								density: elements.density,
-								summary: elements.summary,
-								category: elements.category,
-								phase: elements.phase,
-								appearance: elements.appearance,
-								discovered_by: elements.discovered_by,
-								melt: elements.melt,
-								boil: elements.boil,
-								xpos: elements.xpos,
-								ypos: elements.ypos	
-							}
-						]
-					}
-				}
-			}
-		}
-		else {
-			return null
-		}
-	}
+
+        return elements
+    }
 
 	buttonClick(value) {
 		let element = this.searchElement(this.formatString(value));
 
-		if (element == null) {
-			this.updateLabel(element);
-		}
-		else {
-			this.updateLabel(element.properties[0]);
-		}
+		this.updateLabel(element[0])
 	}
 }
